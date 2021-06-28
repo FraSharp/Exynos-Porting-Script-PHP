651ed163432599b568143558531c54f460eb4c2d
FraSharp <s23265@iisve.it>
2021-06-28 16:22:53 +0200

    optimize? and add some comments

--- port.php
+++ port.php
@@ -1,17 +1,24 @@
 <?php
 
+// list of files to delete
 $list = "list.txt";
-
 $libHwDir = "lib/hw/";
 $lib64HwDir = "lib64/hw/";
+
 echo "x Samsung exynos renaming script\n\n";
 $soc = readline("x SoC name: ");
 echo "\n\n";
 
+// dirs where the files to rename are
+$dir = array(
+    $libHwDir => $dir1,
+    $lib64HwDir => $dir2
+);
 
-RenameToSoC($libHwDir);
-echo "\n";
-RenameToSoC($lib64HwDir);
+for ($i = 0; $i < count($dir); $i++) {
+    RenameToSoC($dir[$i]);
+}
+
 echo "\n";
 DeleteFiles($list);
 echo "\n";
@@ -25,14 +32,19 @@ function RenameToSoC($dir) {
     $files = scandir($dir);
 
     foreach ($files as $file) {
+        // rename that file if has exynos or universal in the name
         if (str_contains($file, ".exynos") or str_contains($file, ".universal")) {
             echo $dir . $file . " found, renaming to $soc\n";
+            // separate name file when a . is found
             $filename = explode(".", $file);
+            // check every position in the exploded filename
             for ($i = 0; $i < count($filename); $i++) {
+                // if one position contains exynos then rename to the desired soc
                 if (str_contains($filename[$i], "exynos")) {
                     $filename[$i] = "exynos" . $soc;
                     rename($dir . $file, $dir . implode(".", $filename));
                 } else {
+                    // if one position contains universal then rename to the desired soc
                     if (str_contains($filename[$i], "universal")) {
                         $filename[$i] = "universal" . $soc;
                         rename($dir . $file, $dir . implode(".", $filename));
